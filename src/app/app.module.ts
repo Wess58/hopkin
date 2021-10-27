7897856a26469ac3e37f43ab71b8dce15df2d8e8
wesley mbate <wmbate@meliora.tech>
2021-10-27 22:42:57 +0300

    added image new assets added content to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
